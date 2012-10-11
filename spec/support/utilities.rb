24d8fad2f0e1c05cfceecbc28d06e77ea81de395
Dino Poblete <dino.poblete@aestrea.com>
2012-10-11 17:46:32 +0800

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,11 +1,24 @@
 include ApplicationHelper
 
+# DEFINE METHODS #
+
 def valid_signin(user)
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
 end
 
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
+
+
+# DEFINE MATCHERS #
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: message)
@@ -35,6 +48,20 @@ RSpec::Matchers.define :have_profile_link do
     page.should have_link('Profile', href: user_path(user))
   end
 end
+
+RSpec::Matchers.define :have_users_link do 
+  match do |page|
+    page.should have_link('Users', href: users_path)
+  end
+end
+
+
+RSpec::Matchers.define :have_settings_link do 
+  match do |page|
+    page.should have_link('Settings', href: edit_user_path(user))
+  end
+end
+
 
 RSpec::Matchers.define :have_signout_link do 
   match do |page|
